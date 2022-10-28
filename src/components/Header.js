d12ae0e169207fd3ef01147036909d12d52a5fa5
Jeffrey <jeff.ho@hotmail.com>
2022-10-28 11:16:42 -0400

    Added pop up modal for youtube cooking recipe

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 export default function Header({name, image}) {
   return (
     <section className="header">
-      <h1>Jeff's Random Meal Generator</h1>
+      <h1>Bee's Random Meal Generator</h1>
       {!name && !image && <h3>Click the button and see what we're having for dinner today!</h3>}
     </section>
   )
